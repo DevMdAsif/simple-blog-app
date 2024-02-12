a78929da1be1c9d454ae247b98810f1b1bd1014e
DevMdAsif <mdasifullah456@gmail.com>
2024-02-12 18:49:56 +0600

    create the footer commponent

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../pages/shared/header/Header";
+import FooterCom from "../pages/shared/footer/footer";
 
 function Main() {
   return (
     <div>
       <Header />
       <Outlet />
+      <FooterCom />
     </div>
   );
 }
